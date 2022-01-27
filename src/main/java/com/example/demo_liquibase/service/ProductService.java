84db2cba9da46b6e881f5143493136a86b40c63b
ReyhanPatria <reyhan2patria@gmail.com>
2022-01-27 15:42:58 +0700

    Fixed repository injection

--- src/main/java/com/example/demo_liquibase/service/ProductService.java
+++ src/main/java/com/example/demo_liquibase/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.example.demo_liquibase.service;
 
 import com.example.demo_liquibase.model.entity.Product;
+import com.example.demo_liquibase.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,17 +11,17 @@ import java.util.Optional;
 @Service
 public class ProductService {
     @Autowired
-    private ProductService productService;
+    private ProductRepository productRepository;
 
     public List<Product> findAll() {
-        return productService.findAll();
+        return productRepository.findAll();
     }
 
     public Optional<Product> findById(Integer id) {
-        return productService.findById(id);
+        return productRepository.findById(id);
     }
 
     public Product save(Product product) {
-        return productService.save(product);
+        return productRepository.save(product);
     }
 }
